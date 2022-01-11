2f17f625f03bd5f01cbedb0b074b73994538cbd9
arlw87 <arlw87@googlemail.com>
2022-01-11 15:28:26 +0000

    Create an output result method that display length of word and number of duplicate characters.

--- Duplicates.java
+++ Duplicates.java
@@ -1,12 +1,17 @@
 import java.util.*;
 
+import javax.swing.text.Style;
+
 public class Duplicates {
 
     public static void main(String ...args){
         System.out.println("Hello Duplicates");
-        int number = countDuplicates("wooord");
+        String word = "wooord";
+        int number = countDuplicates(word);
         System.out.println("Duplicates found: " + number);
-        Map<Character, Integer> result = countAllDuplicates("wooooord");
+        Map<Character, Integer> result = countAllDuplicates(word);
+        outputResults(result, word);
+        System.out.println("End of program");
     }
 
     static int countDuplicates(String s){
@@ -16,14 +21,8 @@ public class Duplicates {
         //convert the string into an array
         char[] array = str1.toCharArray();
 
-        //print out array for debugging purposes
-        System.out.println(Arrays.toString(array));
-
         //sort the array 
         Arrays.sort(array);
-
-        //print out array for debugging purposes
-        System.out.println(Arrays.toString(array));
 
         //count duplicates
         int count = 0;
@@ -55,5 +54,23 @@ public class Duplicates {
         System.out.println(d.toString());
 
         return d;
+    }
+
+    static void outputResults(Map<Character, Integer> results, String s){
+        System.out.println("Here are the Results.....");
+        System.out.println("The length of the String " + s.trim() + " is " + s.length());
+        int numOfDuplicates = 0;
+
+        for(Character c: results.keySet()){
+            if (results.get(c) > 1){
+                numOfDuplicates++;
+                System.out.println("Charcter " + c + " has " + results.get(c) + " occurences");
+            }
+        }
+
+        if (numOfDuplicates == 0){
+            System.out.println("No characters had duplicates");
+        }
+        
     }
 }
