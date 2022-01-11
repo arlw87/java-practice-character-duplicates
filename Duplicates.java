971a84596619e697775331903c78853bd8ddad62
arlw87 <arlw87@googlemail.com>
2022-01-11 15:15:16 +0000

    Add new method that count all unique and duplicate characters and saves to Map

--- Duplicates.java
+++ Duplicates.java
@@ -6,6 +6,7 @@ public class Duplicates {
         System.out.println("Hello Duplicates");
         int number = countDuplicates("wooord");
         System.out.println("Duplicates found: " + number);
+        Map<Character, Integer> result = countAllDuplicates("wooooord");
     }
 
     static int countDuplicates(String s){
@@ -33,5 +34,26 @@ public class Duplicates {
         }
 
         return count;
+    }
+    
+    static Map<Character, Integer> countAllDuplicates(String s){
+        
+        //HashMap to hold the results
+        Map<Character, Integer> d = new HashMap<>();
+
+        //convert the String into a char array
+        String trimmed = s.trim();
+        char[] wordArray = trimmed.toCharArray();
+        
+        //loop through char array
+        //Add key-value pair to map if there isnt one for that key
+        //if there is, increment the value by 1
+        for (Character c: wordArray){
+            d.compute(c, (k, v) -> (v == null)? 1 : v + 1);
+        }
+
+        System.out.println(d.toString());
+
+        return d;
     }
 }
