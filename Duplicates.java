23702b73c93b835c6f4481cb5bb7fa922d0eb970
arlw87 <arlw87@googlemail.com>
2022-01-11 13:27:43 +0000

    Feat: Add a countDuplicates method

--- Duplicates.java
+++ Duplicates.java
@@ -1,1 +1,23 @@
-//Test
+import java.util.*;
+
+public class Duplicates {
+
+    public static void main(String ...args){
+        System.out.println("Hello Duplicates");
+        int number = countDuplicates("woord");
+        System.out.println("Duplicates found: " + number);
+    }
+
+    static int countDuplicates(String s){
+        //trim the String so it contains no whitespace characters
+        String str1 = s.trim();
+
+        //convert the string into an array
+        char[] array = str1.toCharArray();
+
+        //print out array for debugging purposes
+        System.out.println(Arrays.toString(array));
+
+        return 0;
+    }
+}
